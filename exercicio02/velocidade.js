a610455179a31a92b78f6d362f1ab76eed87fc28
Viitorsoares <viitorgab.soares@gmail.com>
2023-12-23 13:40:31 -0300

    Atualizei exercício 2 javascript

---
+++ exercicio02/velocidade.js
@@ -0,0 +1,13 @@
+const nomeVeiculo1 = prompt('Nome do PRIMEIRO veículo:')
+const velocidadeVeiculo1 = parseFloat(prompt('Velocidade ' + nomeVeiculo1 + ":"))
+const nomeVeiculo2 = prompt('Nome do SEGUNDO veículo:')
+const velocidadeVeiculo2 = parseFloat(prompt('Velocidade ' + nomeVeiculo2 + ":"))
+
+
+if (velocidadeVeiculo1 > velocidadeVeiculo2) {
+    alert(nomeVeiculo1 + " esta com a velocidade MAIOR que" + " " + nomeVeiculo2 + "!")
+} else if (velocidadeVeiculo1 < velocidadeVeiculo2) {
+    alert(nomeVeiculo2 + " esta com a velocidade MAIOR que" + " " + nomeVeiculo1 + "!")
+} else {
+    alert(nomeVeiculo1 + " e " + nomeVeiculo2 + " estão com a velocidade IGUAL!")
+}
