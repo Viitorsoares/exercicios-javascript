ca78241dc51248c31d2574191554722545c0580f
Viitorsoares <viitorgab.soares@gmail.com>
2023-12-27 13:24:52 -0300

    Criei exercício 7 javascript

---
+++ exercício07/fila.js
@@ -0,0 +1,11 @@
+let fila = ["Mateus", " Marcos ", " Vitor ", " Gabriel ", " Fernanda "]
+
+do {
+   const opcoes = prompt(
+        "Fila de Espera : " + fila + 
+        "\nEscolha as opções: " + 
+        "\n- Novo paciente" + 
+        "\n- Consultar paciente" +
+        "\n- Sair"
+    )
+} while (opcoes !== Sair)
