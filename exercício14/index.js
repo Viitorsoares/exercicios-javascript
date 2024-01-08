0d07cf49553daf21bec567d48b2359d6c5c279b4
Viitorsoares <viitorgab.soares@gmail.com>
2024-01-08 13:12:38 -0300

    atualizando exercício 14

---
+++ exercício14/index.js
@@ -0,0 +1,20 @@
+function createLabel(text, htmlFor) {
+    const label = document.createElement('label')
+    label.htmlFor = htmlFor
+    label.text = text
+    return label 
+}
+
+function createInput(id, value, name, type= 'text', placeholder = '' ) {
+    const input = document.createElement('input')
+    input.id = id
+    input.value = value
+    input.name = name
+    input.type = type
+    input.placeholder = placeholder
+    return input
+}
+
+const addTechBtn = document.getElementById('addTechBtn')
+const devFirm = document.getElementById('devFirm')
+const developers = []
