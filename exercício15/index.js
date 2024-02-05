807b6d42ea18c84c97003763630d5a2f2cdf2280
Viitorsoares <viitorgab.soares@gmail.com>
2024-02-05 12:23:57 -0300

    Exercício 15 JavaScript
    Finalizado

--- exercício15/index.js
+++ exercício15/index.js
@@ -1,13 +1,13 @@
 const average = (...numbers) => {
-    const sum = numbers.reduce((accum, num) => accum, num, 0)
+    const sum = numbers.reduce((accum, num) => accum + num, 0)
     return sum / numbers.length
 }
 
 console.log(`Média Aritimética Simples; ${average(3, 6, 10, 9)}`)
 
 const weightedAverage = (...entries) => {
-    const sum = entries.reduce((accum, {number, weight}) => accum + (number * weight), 0)
-    const weightSum = entries.reduce((accum, entry) => accum + entry.weigth, 0)
+    const sum = entries.reduce((accum, {number, weight}) => accum + (number * (weight ?? 1)), 0)
+    const weightSum = entries.reduce((accum, entry) => accum + (entry.weigth ?? 1), 0)
     return sum / weightSum
 }
 
@@ -18,5 +18,27 @@ console.log(`Média Ponderada: ${weightedAverage(
 )}`)
 
 const median = (...numbers) => {
-    const orderedNumbers
+    const orderedNumbers = [...numbers].sort((a, b) => a - b)
+    const middle = Math.floor(orderedNumbers.length / 2)
+    if (orderedNumbers.length % 2 !== 0) {
+        return orderedNumbers[middle]
+    }
+
+    const firstMedian = orderedNumbers[middle - 1]
+    const secondMedian = orderedNumbers[middle]
+    return average(firstMedian, secondMedian)
 }
+
+console.log(`Mediana: ${median(2, 5, 99, 4, 42, 7)}`)
+console.log(`Mediana: ${median(15, 14, 8, 7, 3)}`)
+
+const mode = (...numbers) => {
+    const quantities = numbers.map(num => [
+        num,
+        numbers.filter(n => num === n).length  
+    ])
+    quantities.sort((a, b) => b[1] - a[1])
+    return quantities[0][0]
+}
+
+console.log(`Moda: ${mode(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4)}`)
